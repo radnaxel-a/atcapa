bf247249ee0200cbd6a6d5bdd75c673a42ff0c3a
Alexander Avramov <gowascen@gmail.com>
2019-01-24 18:43:24 +0200

    Include userService and Http module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { WeeklyOverviewComponent } from "src/app/weekly-overview/weekly-overview.component";
 import { UsersComponent } from "src/app/users/users.component";
+import { HomeComponent } from "src/app/home/home.component";
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'users', component: UsersComponent },
-  { path: 'weeklyOverview', component: WeeklyOverviewComponent }
+  { path: 'weeklyOverview', component: WeeklyOverviewComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule }    from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { WeeklyOverviewComponent } from './weekly-overview/weekly-overview.component';
 import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WeeklyOverviewComponent,
-    UsersComponent
+    UsersComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
