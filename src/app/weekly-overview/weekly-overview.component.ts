fef9cfc67ef0d67197a11d8df9cee1fd843d825c
Alexander Avramov <gowascen@gmail.com>
2019-01-27 00:35:34 +0200

    List TimeEntries.

--- src/app/weekly-overview/weekly-overview.component.ts
+++ src/app/weekly-overview/weekly-overview.component.ts
@@ -25,29 +25,48 @@ export class WeeklyOverviewComponent implements OnInit {
 
   constructor(weeklyService: WeeklyOverviewService) {
     this.weeklyService = weeklyService;
-   }
-
-  ngOnInit() {
-    this.getData();
   }
 
-  private getData(): void {
+  ngOnInit() {
+  }
+
+  private getData(dateRange): void {
     this.weeklyService.getTimeEntries().subscribe((data) => {
-      this.setTimeEntries(data);
+
+
+      let filteredData = this.filterEntries(dateRange, data);
+      if (filteredData.length != 0) {
+        this.setTimeEntries(filteredData);  
+      }
     })
   }
 
+  private filterEntries(dateRange, data): TimeEntry[] {
+
+    let filteredData = [];
+    let currentEntryDate;
+
+    for (let entry of data["data"]) {
+      currentEntryDate = new Date(entry.date).getTime();
+
+      if (currentEntryDate >= dateRange.min &&
+          currentEntryDate <= dateRange.max) {
+            filteredData.push(this.setTimeEntryType(entry));
+      }
+    }
+    return filteredData;
+  }
+
   private setTimeEntries(data): void {
-    let timeEntries = data["data"];
-    console.log(timeEntries);
+
     let date;
 
-    for (let entry of timeEntries) {
-      date = new Date(entry['date']);
+    for (let entry of data) {
+      
+      date = new Date(entry.date);
 
       this.serializeEntry(date.getDay(), entry);
     }
-    console.log(this.fridayEntries);
   }
 
   private serializeEntry(day, entry): void {
@@ -56,11 +75,8 @@ export class WeeklyOverviewComponent implements OnInit {
       case 0:
         this.sundayEntries.push(entry);
         break;
-      
+
       case 1:
-      entry["time_entry_type"] = {
-        name: "Normal Time"
-      }
         this.mondayEntries.push(entry);
         break;
 
@@ -81,11 +97,52 @@ export class WeeklyOverviewComponent implements OnInit {
         break;
 
       case 6:
-      this.saturdayEntries.push(entry);
+        this.saturdayEntries.push(entry);
         break;
-    
+
       default:
         break;
     }
+  }
+
+  private setTimeEntryType(entry): TimeEntry {
+
+    if (entry.time_entry_type_id === null) {
+      entry.time_entry_type = {
+        name: "Holiday"
+      }
+    }
+
+    return entry;
+  }
+
+  public handleEvent(element): void {
+
+    let range = element.value.split(" - ");
+
+    let firstDate = new Date(range[0]);
+    let secDate = new Date(range[1])
+
+    firstDate.setDate(firstDate.getDate() + 1);
+    secDate.setDate(secDate.getDate() + 1);
+
+    let dateRange = {
+      min: firstDate.getTime(),
+      max: secDate.getTime()
+    }
+
+    this.clearArrays();
+    this.getData(dateRange)
+  }
+
+
+  private clearArrays(): void{
+    this.mondayEntries = [];
+    this.tuesdayEntries = [];
+    this.wednesdayEntries = [];
+    this.thursdayEntries = [];
+    this.fridayEntries = [];
+    this.saturdayEntries = [];
+    this.sundayEntries = [];
   }
 }
