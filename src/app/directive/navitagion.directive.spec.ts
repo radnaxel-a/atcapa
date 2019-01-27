5e03634dce5588fe88541813f86981a3bc3baa9f
Alexander Avramov <gowascen@gmail.com>
2019-01-27 02:32:15 +0200

    Remove .spec files

--- src/app/directive/navitagion.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NavitagionDirective } from './navitagion.directive';
-
-describe('NavitagionDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NavitagionDirective();
-    expect(directive).toBeTruthy();
-  });
-});
